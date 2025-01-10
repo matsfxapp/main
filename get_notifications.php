56080420df65d6aaf8c5915843fdb22d0fcdf050
Mathis <mathiisprv@gmail.com>
2025-01-10 15:55:25 +0100

    Update get_notifications.php

--- get_notifications.php
+++ get_notifications.php
@@ -1,4 +1,4 @@
-<?php
+ /* <?php
 header('Content-Type: application/json');
 require_once 'config.php';
 require_once 'NotificationSystem.php';
@@ -9,4 +9,4 @@ try {
     echo json_encode(['success' => true, 'notifications' => $notifications]);
 } catch (Exception $e) {
     echo json_encode(['success' => false, 'error' => $e->getMessage()]);
-}
+} */
