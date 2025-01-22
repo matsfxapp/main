72923b19848fa7436c1ee14218f7d086d9a7dd9a
Mathis <mathiisprv@gmail.com>
2025-01-22 15:02:49 +0100

    Update test.php

--- test.php
+++ test.php
@@ -2,18 +2,10 @@
 <html lang="de">
 <head>
     <meta charset="UTF-8">
+    <meta name="monetag" content="5b5da452bb7f578199b5f1d963c7b3bf">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ads Test</title>
-    <script>
-        (function(d, z, s) {
-            s.src = 'https://' + d + '/400/' + z;
-            try {
-                (document.head || document.documentElement).appendChild(s);
-            } catch (e) {
-                console.error("Fehler beim Einbetten des Scripts:", e);
-            }
-        })('vemtoutcheeg.com', 8833185, document.createElement('script'));
-    </script>
+    <script>(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('vemtoutcheeg.com',8833185,document.createElement('script'))</script>
 </head>
 <body>
     <h1>Testpage for ads</h1>
