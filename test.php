5232957afbbf6b0a369c87adb017b930010a90eb
Mathis <mathiisprv@gmail.com>
2025-01-22 15:00:32 +0100

    ads test

---
+++ test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ads Test</title>
+    <script>
+        (function(d, z, s) {
+            s.src = 'https://' + d + '/400/' + z;
+            try {
+                (document.head || document.documentElement).appendChild(s);
+            } catch (e) {
+                console.error("Fehler beim Einbetten des Scripts:", e);
+            }
+        })('vemtoutcheeg.com', 8833185, document.createElement('script'));
+    </script>
+</head>
+<body>
+    <h1>Testpage for ads</h1>
+    <p>if it works the ad will be displayed.</p>
+</body>
+</html>
