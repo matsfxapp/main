1e3972bdf194b846598aaf677f9c345f1063965c
Mathis <mathiisprv@gmail.com>
2025-01-25 13:20:25 +0100

    Update test.php

--- test.php
+++ test.php
@@ -5,7 +5,7 @@
     <meta name="monetag" content="5b5da452bb7f578199b5f1d963c7b3bf">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ads Test</title>
-  </head>
+</head>
   <script>(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('vemtoutcheeg.com',8833185,document.createElement('script'))</script>
 <body>
     <h1>Testpage for ads</h1>
