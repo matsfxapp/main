abed8b8925d3e7d6606671368e3ef9848326f9a3
Mathis <mathiisprv@gmail.com>
2025-03-23 00:59:30 +0100

    Update alpha_0.6.2 ⚙️

--- includes/header.php
+++ includes/header.php
@@ -27,7 +27,7 @@ if (isset($_SESSION['user_id'])) {
             <a href="/" class="brand">
                 <div class="logo-wrapper">
                     <img src="/app_logos/matsfx_logo.png" alt="Logo" class="logo-image">
-                    <span class="brand-text">alpha_0.6</span>
+                    <span class="brand-text">alpha_0.6.1</span>
                 </div>
             </a>
 
