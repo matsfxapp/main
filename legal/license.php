ae1b8ba1191a100ac8db1413c9ddcf1cb0fb60fd
Mathis <mathiisprv@gmail.com>
2025-03-31 15:45:13 +0200

    Update license.php

--- legal/license.php
+++ legal/license.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'config/config.php';
+require_once '../config/config.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,11 +7,11 @@ require_once 'config/config.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="matSFX - The new way to listen with Joy! Ad-free and Open-Source, can it be even better?" />
-	<meta property="og:title" content="matSFX - Listen with Joy!" />
-	<meta property="og:description" content="Experience ad-free music, unique Songs and Artists, a new and modern look!" />
-	<meta property="og:image" content="https://alpha.matsfx.com/app_logos/matsfx_logo.png" />
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://matsfx.com/" />
+    <meta property="og:title" content="matSFX - Listen with Joy!" />
+    <meta property="og:description" content="Experience ad-free music, unique Songs and Artists, a new and modern look!" />
+    <meta property="og:image" content="https://alpha.matsfx.com/app_logos/matsfx_logo.png" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://matsfx.com/" />
     <title>matSFX License</title>
     <link rel="icon" type="image/png" href="/app_logos/matsfx_logo.png">
     <link rel="shortcut icon" type="image/png" href="/app_logos/matsfx_logo.png">
