9d6f0b26ebbce2958b0a645b8572ead509b8a33a
Mathis <mathiisprv@gmail.com>
2025-04-01 00:52:46 +0200

    FIXED VENDOR FILE PATH 🗿🔥

--- config/auth.php
+++ config/auth.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'config.php';
-require 'vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
